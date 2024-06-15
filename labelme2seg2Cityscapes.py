c889baf4622ba534a125976d845dc397e41668cf
Hongmin Mu <57067148+HongminMu@users.noreply.github.com>
2024-06-15 14:50:06 +0800

    Update labelme2seg2Cityscapes.py

--- labelme2seg2Cityscapes.py
+++ labelme2seg2Cityscapes.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# --input_dir C:\Users\11385\Desktop\LLimages4_2
 
 from __future__ import print_function
 
